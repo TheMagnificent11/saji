5ba264a380106f1d2773e50ebc63ddffae402f0f
Saji Weerasingham <saji.weerasingham@outlook.com>
2024-06-02 03:48:43 +1000

    update build workflow .net version

--- src/Lewee.IntegrationTests/WebApiIntegrationTests.cs
+++ src/Lewee.IntegrationTests/WebApiIntegrationTests.cs
@@ -16,7 +16,7 @@ namespace Lewee.IntegrationTests;
 /// </summary>
 /// <typeparam name="TEntryPoint">ASP.Net app entry point class</typeparam>
 /// <typeparam name="TFactory">Web application factory type</typeparam>
-/// <typeparam name="TDbContextFixture">Database context fixure type</typeparam>
+/// <typeparam name="TDbContextFixture">Database context fixture type</typeparam>
 /// <typeparam name="TDbContext">Database context type</typeparam>
 /// <typeparam name="TDbSeeder">Database seeder type</typeparam>
 public abstract class WebApiIntegrationTests<TEntryPoint, TFactory, TDbContextFixture, TDbContext, TDbSeeder>
@@ -133,7 +133,7 @@ public abstract class WebApiIntegrationTests<TEntryPoint, TFactory, TDbContextFi
     /// <summary>
     /// Deserializes a HTTP response
     /// </summary>
-    /// <typeparam name="T">Reponse body type</typeparam>
+    /// <typeparam name="T">Response body type</typeparam>
     /// <param name="response">Response to deserialize</param>
     /// <param name="isSuccess">Whether the response was expected successful</param>
     /// <returns>The deserialized data</returns>
